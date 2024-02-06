93eb9cf449ce9b99af5260218594ed0b5fbc5f5b
NDrms <namedimas831@gmail.com>
2024-02-06 13:32:04 +0300

    main

--- app/src/main/java/com/example/gpsreminder/activityCreate/CreateActivity.java
+++ app/src/main/java/com/example/gpsreminder/activityCreate/CreateActivity.java
@@ -4,8 +4,14 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import com.example.gpsreminder.R;
 import com.example.gpsreminder.databinding.ActivityCreateBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CreateActivity extends AppCompatActivity {
 
@@ -25,6 +31,15 @@ public class CreateActivity extends AppCompatActivity {
                 CreateActivity.super.onBackPressed();
             }
         });
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_create);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
 }
