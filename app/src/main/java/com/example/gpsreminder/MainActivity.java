9acca89279a742cace1b37f58757260456fd29e3
NameD <NameDimas831@gmail.com>
2024-04-13 07:56:58 +0300

    main

--- app/src/main/java/com/example/gpsreminder/MainActivity.java
+++ app/src/main/java/com/example/gpsreminder/MainActivity.java
@@ -1,18 +1,12 @@
 package com.example.gpsreminder;
 
-import android.app.Activity;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -21,34 +15,23 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.gpsreminder.activityCreate.CreateActivity;
 import com.example.gpsreminder.databinding.ActivityMainBinding;
-import com.example.gpsreminder.storage.RemindEx;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String APP_PREFERENCES = "appSettings";
-    public static final String APP_PREFERENCES_PREMIUM = "true";
     private AppBarConfiguration mAppBarConfiguration;
-    SharedPreferences mSettings;
-    private ActivityMainBinding binder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
-
-
-        binder = ActivityMainBinding.inflate(getLayoutInflater());
+        com.example.gpsreminder.databinding.ActivityMainBinding binder = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binder.getRoot());
 
 
         setSupportActionBar(binder.appBarMain.toolbar);
-        binder.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, CreateActivity.class);
-                startActivity(intent);
-            }
+        binder.appBarMain.fab.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CreateActivity.class);
+            startActivity(intent);
         });
 
         DrawerLayout drawer = binder.drawerLayout;
@@ -63,12 +46,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
         //Data binding тест
-        RemindEx remindEx = new RemindEx("Тест", "10:00");
 
-
-
-        //ActivityMainBinding activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());//DataBindingUtil.setContentView(this,R.layout.activity_main);
-        //activityMainBinding.setRem(RemindEx);
 
         createNotificationChannel();
     }
@@ -76,17 +54,15 @@ public class MainActivity extends AppCompatActivity {
     private void createNotificationChannel() {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is not in the Support Library.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "mychannel";
-            String description = "mychannel";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("mychannel", name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this.
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
+        CharSequence name = "mychannel";
+        String description = "mychannel";
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel("mychannel", name, importance);
+        channel.setDescription(description);
+        // Register the channel with the system; you can't change the importance
+        // or other notification behaviors after this.
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
     }
 
 
--- app/src/main/java/com/example/gpsreminder/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/gpsreminder/ui/gallery/GalleryFragment.java
@@ -1,10 +1,5 @@
 package com.example.gpsreminder.ui.gallery;
 
-import static com.example.gpsreminder.MainActivity.APP_PREFERENCES_PREMIUM;
-
-import android.content.Context;
-import com.example.gpsreminder.MainActivity.*;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,66 +8,53 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.gpsreminder.databinding.FragmentPremiumBinding;
 
 public class GalleryFragment extends Fragment {
 
-    SharedPreferences mSettings;
     private FragmentPremiumBinding binding;
     private int c;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        GalleryViewModel galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
 
         binding = FragmentPremiumBinding.inflate(inflater, container, false);
-//            if (mSettings.getString(APP_PREFERENCES_PREMIUM, "")){
-//            binding.premium.setVisibility(View.GONE);
-//        }
-        View root = binding.getRoot();
-        binding.premium.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                String j;
-                int i = c;
-                switch (i){
-                    case 0:
-                        j = "Премиум появится в следующих обновлениях";
-                        break;
-                    case 1:
-                        j = "Премиума пока нет";
-                        break;
-                    case 2:
-                        j = "Эй, почему бы тебе не создать еще одно напоминание?";
-                        break;
-                    case 3:
-                        j = "Обновление еще не вышло";
-                        break;
-                    case 4:
-                        j = "Я не могу дать тебе то, чего нет";
-                        break;
-                    case 5:
-                        j = "Потрать свое время продуктивнее";
-                        break;
-                    case 6:
-                        j = "Последнее предупреждение 😡";
-                        break;
-                    default:
-                        j = "Оно того стоило?";
-                        binding.premium.setVisibility(View.GONE);
-                        SharedPreferences.Editor editor = mSettings.edit();
-                        editor.putString(APP_PREFERENCES_PREMIUM, "false");
-                        editor.apply();
-                        break;
-                }
-                Toast toast = Toast.makeText(getContext(),j, Toast.LENGTH_LONG);
-                toast.show();
-                c++;
+        View root = binding.getRoot();
+        binding.premium.setOnClickListener(v -> {
+            String j;
+            int i = c;
+            switch (i){
+                case 0:
+                    j = "Премиум появится в следующих обновлениях";
+                    break;
+                case 1:
+                    j = "Премиума пока нет";
+                    break;
+                case 2:
+                    j = "Эй, почему бы тебе не создать еще одно напоминание?";
+                    break;
+                case 3:
+                    j = "Обновление еще не вышло";
+                    break;
+                case 4:
+                    j = "Я не могу дать тебе то, чего нет";
+                    break;
+                case 5:
+                    j = "Потрать свое время продуктивнее";
+                    break;
+                case 6:
+                    j = "Последнее предупреждение 😡";
+                    break;
+                default:
+                    j = "Оно того стоило?";
+                    binding.premium.setVisibility(View.GONE);
+                    break;
             }
+            Toast toast = Toast.makeText(getContext(),j, Toast.LENGTH_SHORT);
+            toast.show();
+            c++;
         });
         return root;
     }
